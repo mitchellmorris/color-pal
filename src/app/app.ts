8b656db7768ca7148029982e21708a41efe05b27
Mitchell <mitchellwaynemorris@gmail.com>
2026-02-19 17:16:35 -0800

    refactor(palettes): loading service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingWrapper } from '@components/state';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
@@ -7,6 +8,7 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     RouterOutlet,
     ButtonModule,
+    LoadingWrapper,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
--- src/app/pages/palettes/palettes.ts
+++ src/app/pages/palettes/palettes.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoadingWrapper } from '@components';
 
 @Component({
   selector: 'app-palettes',
   imports: [
     RouterOutlet,
-    LoadingWrapper
   ],
   templateUrl: './palettes.html',
   styleUrl: './palettes.css',
